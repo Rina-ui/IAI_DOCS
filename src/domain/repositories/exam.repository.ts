35ec9a3ee1232d761675e4313710c076c48fb0cd
Rina-ui <marinagracia895@gmail.com>
2026-03-26 23:34:04 +0000

    mise en place de l'architecture clean et start the job

---
+++ src/domain/repositories/exam.repository.ts
@@ -0,0 +1,9 @@
+import { Exam } from '../entities/exam.entity';
+
+export interface IExamRepository {
+  findById(id: string): Promise<Exam | null>;
+  findByTitle(title: string): Promise<Exam | null>;
+  findAll(filters?: { level?: string; subject?: string }): Promise<Exam | null>;
+  save(exam: Exam): Promise<Exam>;
+  findPending(): Promise<Exam[]>;
+}
